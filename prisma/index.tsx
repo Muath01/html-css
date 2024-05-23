1e1fbf51971be920450688bbf6295efcab47aaef
muath <muath.khalifa@yahoo.com>
2024-05-23 01:29:45 +0100

    added a try and catch to the createUserFunction

--- prisma/index.tsx
+++ prisma/index.tsx
@@ -5,22 +5,25 @@ const prisma = new PrismaClient();
 
 export async function createUser(email: string, score: number) {
   console.log("in the create User function: ", score);
-  const user = await prisma.user.create({
-    data: {
-      email: email,
-      country: "LY",
-      dob: "21-01-21",
-      first_name: "Muath",
-      second_name: "Khalifa",
-      Gender: "M",
-      salary_range: "20,000-30,000",
-      score,
-    },
-  });
 
-  console.log("created: ", user);
-
-  return user.id;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: email,
+        country: "LY",
+        dob: "21-01-21",
+        first_name: "Muath",
+        second_name: "Khalifa",
+        Gender: "M",
+        salary_range: "20,000-30,000",
+        score,
+      },
+    });
+    console.log("created: ", user);
+    return user.id;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function upadeteEmailSent(userId: string) {
